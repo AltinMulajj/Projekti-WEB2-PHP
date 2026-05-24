23231e4ca5a26b1cca66a2faa5a21370e0f3d583
drenkelmendi02 <dren.kelmendi2@student.uni-pr.edu>
2026-05-24 20:46:27 +0200

    Update Product.php
    Added product listing and search functionality with SQL filters

--- phase1/classes/Product.php
+++ phase1/classes/Product.php
@@ -51,3 +51,39 @@ private static function db()
 
     return self::$conn;
 }
+public static function all(array $filters = []): array
+{
+    $conn = self::db();
+
+    $sql = "SELECT * FROM products WHERE 1=1";
+
+    if (!empty($filters['q'])) {
+
+        $search = mysqli_real_escape_string($conn, $filters['q']);
+
+        $sql .= " AND (
+            name LIKE '%$search%' 
+            OR description LIKE '%$search%' 
+            OR category LIKE '%$search%'
+        )";
+    }
+
+    if (!empty($filters['category']) && $filters['category'] !== 'all') {
+
+        $category = mysqli_real_escape_string($conn, $filters['category']);
+
+        $sql .= " AND category = '$category'";
+    }
+
+    $sql .= " ORDER BY id DESC";
+
+    $result = mysqli_query($conn, $sql);
+
+    $products = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $products[] = $row;
+    }
+
+    return $products;
+}
